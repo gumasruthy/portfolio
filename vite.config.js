79ed7237409fe46b3c009a60de6c120c69988c8f
gumasruthy <gumasruthy@vt.edu>
2024-12-25 18:42:58 -0500

    Update configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  
+  base: '/portfolio/',  
   build: {
-    outDir: 'docs',
-    emptyOutDir: true,
+    outDir: 'dist',
+    assetsDir: 'assets',
   }
 })
